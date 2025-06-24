869c973d6c8df9188563ef2f04ba3d4741e49b32
Sufyaan1301 <sufyaan9877@gmail.com>
2025-06-24 21:56:39 +0700

    favorite Bloc and Api

--- lib/model/Like_response.dart
+++ lib/model/Like_response.dart
@@ -1,65 +1,100 @@
-class LikeResponse {
-  final String message;
-  final bool liked;
+import 'dart:convert';
 
-  LikeResponse({
-    required this.message,
-    required this.liked,
+class PostLiked {
+  final String? message;
+  final bool? liked;
+
+  PostLiked({
+    this.message,
+    this.liked,
   });
 
-  factory LikeResponse.fromJson(Map<String, dynamic> json) {
-    return LikeResponse(
-      message: json['message'] ?? '',
-      liked: json['liked'] ?? false,
-    );
-  }
+  factory PostLiked.fromJson(String str) => PostLiked.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory PostLiked.fromMap(Map<String, dynamic> json) => PostLiked(
+        message: json["message"],
+        liked: json["liked"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "message": message,
+        "liked": liked,
+      };
 }
 
-class FavoriteHewanResponse {
-  final String message;
-  final List<FavoriteHewanData> data;
+class GetLiked {
+  final String? message;
+  final List<Datum>? data;
 
-  FavoriteHewanResponse({required this.message, required this.data});
-
-  factory FavoriteHewanResponse.fromJson(Map<String, dynamic> json) {
-    return FavoriteHewanResponse(
-      message: json['message'] ?? '',
-      data: (json['data'] as List<dynamic>)
-          .map((e) => FavoriteHewanData.fromJson(e))
-          .toList(),
-    );
-  }
-}
-
-class FavoriteHewanData {
-  final int id;        // id record favorit
-  final int hewanId;   // id hewan yang di-like
-  final String image;
-  final String nama;
-  final String status;
-  final int likesCount;
-
-  FavoriteHewanData({
-    required this.id,
-    required this.hewanId,
-    required this.image,
-    required this.nama,
-    required this.status,
-    required this.likesCount,
+  GetLiked({
+    this.message,
+    this.data,
   });
 
-  factory FavoriteHewanData.fromJson(Map<String, dynamic> json) {
-    return FavoriteHewanData(
-      id: json['id'] ?? 0,            // id tabel favorit
-      hewanId: json['hewan_id'] ?? 0, // ini ambil dari kolom hewan_id
-      image: json['image'] ?? '',
-      nama: json['nama'] ?? '',
-      status: json['status'] ?? '',
-      likesCount: json['likes_count'] ?? 0,
-    );
-  }
+  factory GetLiked.fromJson(String str) => GetLiked.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory GetLiked.fromMap(Map<String, dynamic> json) => GetLiked(
+        message: json["message"],
+        data: json["data"] == null
+            ? []
+            : List<Datum>.from(json["data"]!.map((x) => Datum.fromMap(x))),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "message": message,
+        "data":
+            data == null ? [] : List<dynamic>.from(data!.map((x) => x.toMap())),
+      };
 }
 
+class Datum {
+  final String? image;
+  final String? nama;
+  final Status? status;
+  final int? likesCount;
 
+  Datum({
+    this.image,
+    this.nama,
+    this.status,
+    this.likesCount,
+  });
 
+  factory Datum.fromJson(String str) => Datum.fromMap(json.decode(str));
 
+  String toJson() => json.encode(toMap());
+
+  factory Datum.fromMap(Map<String, dynamic> json) => Datum(
+        image: json["image"],
+        nama: json["nama"],
+        status: statusValues.map[json["status"]]!,
+        likesCount: json["likes_count"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "image": image,
+        "nama": nama,
+        "status": statusValues.reverse[status],
+        "likes_count": likesCount,
+      };
+}
+
+enum Status { TERSEDIA }
+
+final statusValues = EnumValues({"tersedia": Status.TERSEDIA});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
